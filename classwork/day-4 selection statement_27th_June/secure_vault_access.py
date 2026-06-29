19b40f97330813f2c1c4f1ee800f7b901b9999d1
Sakshimishra2006 <24cse360@ipec.org.in>
2026-06-29 23:41:47 +0530

    Initial Commit

---
+++ classwork/day-4 selection statement_27th_June/secure_vault_access.py
@@ -0,0 +1,35 @@
+'''---------- Secure Vault Access ------------------
+A digital vault can only be opened if the user
+enters the correct security code.
+
+Write a Python program that accepts the entered
+security code.
+
+If the entered code is 7890, display:
+Access Granted to the Vault.
+
+Otherwise, do nothing.
+----------------------------------------------------
+Sample Input
+Enter Security Code : 7890
+----------------------------------------------------
+Sample Output
+Access Granted to the Vault.
+----------------------------------------------------'''
+#----------------------------------------------------
+
+# input of security code
+code = int(input("Enter Security Code : "))
+
+# checking security code
+if(code == 7890):
+    print("Access Granted to the Vault.")
+else:
+    print("Access Denied!")
+
+#----------------------------------------------------
+
+''' Output :
+Enter Security Code : 7890
+Access Granted to the Vault.
+'''
